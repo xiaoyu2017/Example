be8c66a4337b7512ded3056d8e23f654aa9cf182
xiaoyu <jiangzhong91@gmail.com>
2023-01-02 15:38:22 +0800

    sql

--- BookManager/db.sql
+++ BookManager/db.sql
@@ -5,11 +5,11 @@ use BookManager;
 create table category
 (
     `id`         int AUTO_INCREMENT,
-    `createTime` timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updateTime` timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `sort`       int(1)      NOT NULL DEFAULT 0,
-    `status`     tinyint(1)  NOT NULL DEFAULT 1,
-    `name`       varchar(20) NOT NULL,
+    `createTime` timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updateTime` timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `sort`       int(1)             NOT NULL DEFAULT 0,
+    `status`     tinyint(1)         NOT NULL DEFAULT 1,
+    `name`       varchar(20) UNIQUE NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
